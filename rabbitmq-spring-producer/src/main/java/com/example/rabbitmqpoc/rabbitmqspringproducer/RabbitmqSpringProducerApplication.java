3b51d6b42182fc993b820effa24cc90d18ecc876
Levindo Neto <levindogtn@gmail.com>
2018-10-25 11:58:08 -0300

    Add producer package

---
+++ rabbitmq-spring-producer/src/main/java/com/example/rabbitmqpoc/rabbitmqspringproducer/RabbitmqSpringProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.rabbitmqpoc.rabbitmqspringproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpringProducerApplication.class, args);
+	}
+}
