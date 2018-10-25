94faffb584fb9c03d1e599c1a612194df76dbc37
Levindo Neto <levindogtn@gmail.com>
2018-10-25 11:58:45 -0300

    Add consumer initialization package

---
+++ rabbitmq-spring-consumer/src/main/java/com/example/rabbitmqpoc/rabbitmqspringconsumer/RabbitmqSpringConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.example.rabbitmqpoc.rabbitmqspringconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqSpringConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqSpringConsumerApplication.class, args);
+	}
+}
---
+++ rabbitmq-spring-consumer/src/test/java/com/example/rabbitmqpoc/rabbitmqspringconsumer/RabbitmqSpringConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rabbitmqpoc.rabbitmqspringconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqSpringConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
